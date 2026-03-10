07609a2b1b3910631466f2389dcb787d6619f0b7
jincy-joy <minnuvjoy@gmail.com>
2026-03-10 10:46:06 +0530

    Initial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps CI/CD Pipeline Project Running Successfully!");
+  res.send("DevOps Project Running Successfully!");
 });
 
 app.listen(3000, () => {
